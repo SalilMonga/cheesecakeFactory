2af9e073a7dabb62b3764e13a52cc80c495eb88e
Mohith Kanthamneni <50568915+Mohith1@users.noreply.github.com>
2025-02-22 23:25:06 -0800

    Update profile.dart to connect to ulisses settings

--- cheesecakefactory/lib/profile.dart
+++ cheesecakefactory/lib/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'settings.dart'
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -82,6 +83,10 @@ class _ProfileScreenState extends State<ProfileScreen>
               onTap: () {
                 _animationController.forward(from: 0);
                 print('Settings tapped');
+                 Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Settings()), // Navigate to Settings page
+                );
               },
               child: AnimatedBuilder(
                 animation: _rotationAnimation,
