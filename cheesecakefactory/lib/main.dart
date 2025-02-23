2aeb30f27eee0b767d4723990d1ce2b745f4d69f
Jax1106AA <158373403+Jax1106AA@users.noreply.github.com>
2025-02-22 22:11:16 -0800

    Noise Page

--- cheesecakefactory/lib/main.dart
+++ cheesecakefactory/lib/main.dart
@@ -4,13 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 // import 'profile.dart';
 import 'NavigationBar.dart' as customNavBar;
+import 'notification_service.dart'; // Import the notification service
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final NotificationService notificationService = NotificationService();
+  notificationService.init();
+  runApp(MyApp(notificationService: notificationService)); // Pass the service to MyApp
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final NotificationService notificationService;
+
+  const MyApp({Key? key, required this.notificationService}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +24,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 30, 156, 28)),
+          seedColor: const Color.fromARGB(255, 30, 156, 28),
+        ),
         useMaterial3: true,
       ),
       home: const customNavBar.NavigationBar(), // Set the home to NavigationBar
@@ -26,55 +33,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-/*import 'package:cheesecakefactory/HomePage.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    statusBarIconBrightness: Brightness.dark,
-  ));
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    //
-
-    //
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ProfileScreen(), // Correct: No const
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 92, 122, 16)),
-        useMaterial3: true,
-      ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: const MyHomePage (title: 'Homepage'),
-    );
-  }
-}*/
